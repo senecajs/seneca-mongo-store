8dd189836a53587a68f152c7f367ad297c57ab0d
EamonnK <amen_cionna@hotmail.com>
2018-01-21 15:13:05 +0000

    Pre-commit hook removed. Code quality control updated - .eslintrc.js added/prettier installed

--- mongo-store.js
+++ mongo-store.js
@@ -1,7 +1,6 @@
 /* Copyright (c) 2010-2015 Richard Rodger, MIT License */
 'use strict'
 
-
 var _ = require('lodash')
 var Mongo = require('mongodb')
 var MongoClient = Mongo.MongoClient
@@ -9,24 +8,20 @@ var ObjectID = Mongo.ObjectID
 
 var name = 'mongo-store'
 
-
 /*
 native$ = object => use object as query, no meta settings
 native$ = array => use first elem as query, second elem as meta settings
 */
 
-
-function idstr (obj) {
-  return (obj && obj.toHexString) ? obj.toHexString() : '' + obj
+function idstr(obj) {
+  return obj && obj.toHexString ? obj.toHexString() : '' + obj
 }
 
-
-function makeid (hexstr) {
+function makeid(hexstr) {
   if (_.isString(hexstr) && 24 === hexstr.length) {
     try {
       return ObjectID.createFromHexString(hexstr)
-    }
-    catch (e) {
+    } catch (e) {
       return hexstr
     }
   }
@@ -34,8 +29,7 @@ function makeid (hexstr) {
   return hexstr
 }
 
-
-function fixquery (qent, q) {
+function fixquery(qent, q) {
   var qq = {}
 
   if (!q.native$) {
@@ -48,16 +42,14 @@ function fixquery (qent, q) {
       qq._id = makeid(qq.id)
       delete qq.id
     }
-  }
-  else {
+  } else {
     qq = _.isArray(q.native$) ? q.native$[0] : q.native$
   }
 
   return qq
 }
 
-
-function metaquery (qent, q) {
+function metaquery(qent, q) {
   var mq = {}
 
   if (!q.native$) {
@@ -78,34 +70,29 @@ function metaquery (qent, q) {
     if (q.fields$) {
       mq.fields = q.fields$
     }
-  }
-  else {
+  } else {
     mq = _.isArray(q.native$) ? q.native$[1] : mq
   }
 
   return mq
 }
 
-
-module.exports = function (opts) {
+module.exports = function(opts) {
   var seneca = this
   var desc
 
   var dbinst = null
   var collmap = {}
 
-
-  function error (args, err, cb) {
+  function error(args, err, cb) {
     if (err) {
-      seneca.log.error('entity', err, {store: name})
+      seneca.log.error('entity', err, { store: name })
       cb(err)
       return true
-    }
-    else return false
+    } else return false
   }
 
-
-  function configure (conf, cb) {
+  function configure(conf, cb) {
     // defer connection
     // TODO: expose connection action
     if (!_.isUndefined(conf.connect) && !conf.connect) {
@@ -115,25 +102,27 @@ module.exports = function (opts) {
     // Turn the hash into a mongo uri
     if (!conf.uri) {
       conf.uri = 'mongodb://'
-      conf.uri += (conf.username) ? conf.username : ''
-      conf.uri += (conf.password) ? ':' + conf.password + '@' : ''
+      conf.uri += conf.username ? conf.username : ''
+      conf.uri += conf.password ? ':' + conf.password + '@' : ''
       conf.uri += conf.host || conf.server
-      conf.uri += (conf.port) ? ':' + conf.port : ''
+      conf.uri += conf.port ? ':' + conf.port : ''
       conf.uri += '/' + (conf.db || conf.name)
     }
 
     // Extend the db options with some defaults
     // See http://mongodb.github.io/node-mongodb-native/2.0/reference/connecting/connection-settings/ for options
-    var options = seneca.util.deepextend({
-      db: {},
-      server: {},
-      replset: {},
-      mongos: {}
-    }, conf.options)
-
+    var options = seneca.util.deepextend(
+      {
+        db: {},
+        server: {},
+        replset: {},
+        mongos: {}
+      },
+      conf.options
+    )
 
     // Connect using the URI
-    MongoClient.connect(conf.uri, options, function (err, db) {
+    MongoClient.connect(conf.uri, options, function(err, db) {
       if (err) {
         return seneca.die('connect', err, conf)
       }
@@ -145,48 +134,43 @@ module.exports = function (opts) {
     })
   }
 
-
-  function getcoll (args, ent, cb) {
-    var canon = ent.canon$({object: true})
+  function getcoll(args, ent, cb) {
+    var canon = ent.canon$({ object: true })
 
     var collname = (canon.base ? canon.base + '_' : '') + canon.name
 
     if (!collmap[collname]) {
-      dbinst.collection(collname, function (err, coll) {
+      dbinst.collection(collname, function(err, coll) {
         if (!error(args, err, cb)) {
           collmap[collname] = coll
           cb(null, coll)
         }
       })
-    }
-    else {
+    } else {
       cb(null, collmap[collname])
     }
   }
 
-
   var store = {
     name: name,
 
-    close: function (args, cb) {
+    close: function(args, cb) {
       if (dbinst) {
         dbinst.close(cb)
-      }
-      else return cb()
+      } else return cb()
     },
 
-
-    save: function (args, cb) {
+    save: function(args, cb) {
       var ent = args.ent
 
       var update = !!ent.id
 
-      getcoll(args, ent, function (err, coll) {
+      getcoll(args, ent, function(err, coll) {
         if (!error(args, err, cb)) {
           var entp = {}
 
           var fields = ent.fields$()
-          fields.forEach(function (field) {
+          fields.forEach(function(field) {
             entp[field] = ent[field]
           })
 
@@ -195,18 +179,19 @@ module.exports = function (opts) {
           }
 
           if (update) {
-            var q = {_id: makeid(ent.id)}
+            var q = { _id: makeid(ent.id) }
             delete entp.id
 
-            coll.updateOne(q, {$set: entp}, {upsert: true}, function (err, update) {
+            coll.updateOne(q, { $set: entp }, { upsert: true }, function(
+              err
+            ) {
               if (!error(args, err, cb)) {
                 seneca.log.debug('save/update', ent, desc)
                 cb(null, ent)
               }
             })
-          }
-          else {
-            coll.insertOne(entp, function (err, inserts) {
+          } else {
+            coll.insertOne(entp, function(err, inserts) {
               if (!error(args, err, cb)) {
                 ent.id = idstr(inserts.ops[0]._id)
 
@@ -219,17 +204,16 @@ module.exports = function (opts) {
       })
     },
 
-
-    load: function (args, cb) {
+    load: function(args, cb) {
       var qent = args.qent
       var q = args.q
 
-      getcoll(args, qent, function (err, coll) {
+      getcoll(args, qent, function(err, coll) {
         if (!error(args, err, cb)) {
           var mq = metaquery(qent, q)
           var qq = fixquery(qent, q)
 
-          coll.findOne(qq, mq, function (err, entp) {
+          coll.findOne(qq, mq, function(err, entp) {
             if (!error(args, err, cb)) {
               var fent = null
               if (entp) {
@@ -247,21 +231,20 @@ module.exports = function (opts) {
       })
     },
 
-
-    list: function (args, cb) {
+    list: function(args, cb) {
       var qent = args.qent
       var q = args.q
 
-      getcoll(args, qent, function (err, coll) {
+      getcoll(args, qent, function(err, coll) {
         if (!error(args, err, cb)) {
           var mq = metaquery(qent, q)
           var qq = fixquery(qent, q)
 
-          coll.find(qq, mq, function (err, cur) {
+          coll.find(qq, mq, function(err, cur) {
             if (!error(args, err, cb)) {
               var list = []
 
-              cur.each(function (err, entp) {
+              cur.each(function(err, entp) {
                 if (!error(args, err, cb)) {
                   if (entp) {
                     var fent = null
@@ -272,8 +255,7 @@ module.exports = function (opts) {
                       fent = qent.make$(entp)
                     }
                     list.push(fent)
-                  }
-                  else {
+                  } else {
                     seneca.log.debug('list', q, list.length, list[0], desc)
                     cb(null, list)
                   }
@@ -285,37 +267,34 @@ module.exports = function (opts) {
       })
     },
 
-
-    remove: function (args, cb) {
+    remove: function(args, cb) {
       var qent = args.qent
       var q = args.q
 
       var all = q.all$ // default false
       var load = _.isUndefined(q.load$) ? true : q.load$ // default true
 
-      getcoll(args, qent, function (err, coll) {
+      getcoll(args, qent, function(err, coll) {
         if (!error(args, err, cb)) {
           var qq = fixquery(qent, q)
 
           if (all) {
-            coll.deleteMany(qq, {}, function (err) {
+            coll.deleteMany(qq, {}, function(err) {
               seneca.log.debug('remove/all', q, desc)
               cb(err)
             })
-          }
-          else {
+          } else {
             var mq = metaquery(qent, q)
-            coll.findOne(qq, mq, function (err, entp) {
+            coll.findOne(qq, mq, function(err, entp) {
               if (!error(args, err, cb)) {
                 if (entp) {
-                  coll.deleteOne({_id: entp._id}, {}, function (err) {
+                  coll.deleteOne({ _id: entp._id }, {}, function(err) {
                     seneca.log.debug('remove/one', q, entp, desc)
 
                     var ent = load ? entp : null
                     cb(err, ent)
                   })
-                }
-                else cb(null)
+                } else cb(null)
               }
             })
           }
@@ -323,37 +302,33 @@ module.exports = function (opts) {
       })
     },
 
-    native: function (args, done) {
-      dbinst.collection('seneca', function (err, coll) {
+    native: function(args, done) {
+      dbinst.collection('seneca', function(err, coll) {
         if (!error(args, err, done)) {
-          coll.findOne({}, {}, function (err, entp) {
+          coll.findOne({}, {}, function(err) {
             if (!error(args, err, done)) {
               done(null, dbinst)
-            }
-            else {
+            } else {
               done(err)
             }
           })
-        }
-        else {
+        } else {
           done(err)
         }
       })
     }
   }
 
-
   var meta = seneca.store.init(seneca, opts, store)
   desc = meta.desc
 
-
-  seneca.add({init: store.name, tag: meta.tag}, function (args, done) {
-    configure(opts, function (err) {
-      if (err) return seneca.die('store', err, {store: store.name, desc: desc})
+  seneca.add({ init: store.name, tag: meta.tag }, function(args, done) {
+    configure(opts, function(err) {
+      if (err)
+        return seneca.die('store', err, { store: store.name, desc: desc })
       return done()
     })
   })
 
-
-  return {name: store.name, tag: meta.tag}
+  return { name: store.name, tag: meta.tag }
 }
--- test/mongo.test.js
+++ test/mongo.test.js
@@ -8,13 +8,12 @@ var Assert = require('assert')
 var Code = require('code')
 var expect = Code.expect
 
-var lab = exports.lab = Lab.script()
+var lab = (exports.lab = Lab.script())
 var describe = lab.describe
 var it = lab.it
 var before = lab.before
 
 var Shared = require('seneca-store-test')
-
 
 var si = Seneca({
   default_plugins: {
@@ -25,9 +24,8 @@ var si = Seneca({
 si.__testcount = 0
 var testcount = 0
 
-
-describe('mongo tests', function () {
-  before({}, function (done) {
+describe('mongo tests', function() {
+  before({}, function(done) {
     if (si.version >= '2.0.0') {
       si.use('entity')
     }
@@ -38,39 +36,38 @@ describe('mongo tests', function () {
     si.ready(done)
   })
 
-  it('basic test', function (done) {
+  it('basic test', function(done) {
     testcount++
     Shared.basictest(si, done)
   })
 
-  it('extra test', function (done) {
+  it('extra test', function(done) {
     testcount++
     extratest(si, done)
   })
 
-  it('close test', function (done) {
+  it('close test', function(done) {
     Shared.closetest(si, testcount, done)
   })
 })
 
-
-function extratest (si, done) {
+function extratest(si, done) {
   console.log('EXTRA')
 
   Async.series(
     {
-      native: function (cb) {
+      native: function(cb) {
         var foo = si.make$('foo')
-        foo.native$(function (err, db) {
+        foo.native$(function(err, db) {
           Assert.ok(null == err)
 
-          db.collection('foo', function (err, coll) {
+          db.collection('foo', function(err, coll) {
             Assert.ok(null == err)
 
-            coll.find({}, {}, function (err, cursor) {
+            coll.find({}, {}, function(err, cursor) {
               Assert.ok(null == err)
 
-              cursor.each(function (entry) {
+              cursor.each(function(entry) {
                 if (!entry) {
                   cb()
                 }
@@ -80,108 +77,118 @@ function extratest (si, done) {
         })
       },
 
-      native_query: function (cb) {
+      native_query: function(cb) {
         var nat = si.make$('nat')
-        nat.remove$({all$: true}, function (err) {
+        nat.remove$({ all$: true }, function(err) {
           Assert.ok(null == err)
 
           nat.a = 1
-          nat.save$(function (err, nat) {
+          nat.save$(function(err, nat) {
             Assert.ok(null == err)
 
             nat = nat.make$()
             nat.a = 2
-            nat.save$(function (err, nat) {
+            nat.save$(function(err, nat) {
               Assert.ok(null == err)
 
-              nat.list$({native$: [{/* $or:[{a:1},{a:2}]*/}, {sort: [['a', -1]]}]}, function (err, list) {
-                Assert.ok(null == err)
-                Assert.equal(2, list.length)
-                Assert.equal(2, list[0].a)
-                Assert.equal(1, list[1].a)
-                cb()
-              })
+              nat.list$(
+                {
+                  native$: [
+                    {
+                      /* $or:[{a:1},{a:2}]*/
+                    },
+                    { sort: [['a', -1]] }
+                  ]
+                },
+                function(err, list) {
+                  Assert.ok(null == err)
+                  Assert.equal(2, list.length)
+                  Assert.equal(2, list[0].a)
+                  Assert.equal(1, list[1].a)
+                  cb()
+                }
+              )
             })
           })
         })
       },
 
-      remove: function (cb) {
+      remove: function(cb) {
         var cl = si.make$('lmt')
-        cl.remove$({all$: true}, function (err, foo) {
+        cl.remove$({ all$: true }, function(err) {
           Assert.ok(null == err)
           cb()
         })
       },
 
-      insert1st: function (cb) {
+      insert1st: function(cb) {
         var cl = si.make$('lmt')
         cl.p1 = 'v1'
-        cl.save$(function (err, foo) {
+        cl.save$(function(err) {
           Assert.ok(null == err)
           cb()
         })
       },
 
-      insert2nd: function (cb) {
+      insert2nd: function(cb) {
         var cl = si.make$('lmt')
         cl.p1 = 'v2'
-        cl.save$(function (err, foo) {
+        cl.save$(function(err) {
           Assert.ok(null == err)
           cb()
         })
       },
 
-      insert3rd: function (cb) {
+      insert3rd: function(cb) {
         var cl = si.make$('lmt')
         cl.p1 = 'v3'
-        cl.save$(function (err, foo) {
+        cl.save$(function(err) {
           Assert.ok(null == err)
           cb()
         })
       },
 
-      listall: function (cb) {
-        var cl = si.make({name$: 'lmt'})
-        cl.list$({}, function (err, lst) {
+      listall: function(cb) {
+        var cl = si.make({ name$: 'lmt' })
+        cl.list$({}, function(err, lst) {
           Assert.ok(null == err)
           Assert.equal(3, lst.length)
           cb()
         })
       },
 
-      listlimit1skip1: function (cb) {
-        var cl = si.make({name$: 'lmt'})
-        cl.list$({limit$: 1, skip$: 1}, function (err, lst) {
+      listlimit1skip1: function(cb) {
+        var cl = si.make({ name$: 'lmt' })
+        cl.list$({ limit$: 1, skip$: 1 }, function(err, lst) {
           Assert.ok(null == err)
           Assert.equal(1, lst.length)
           cb()
         })
       },
 
-      listlimit2skip3: function (cb) {
-        var cl = si.make({name$: 'lmt'})
-        cl.list$({limit$: 2, skip$: 3}, function (err, lst) {
+      listlimit2skip3: function(cb) {
+        var cl = si.make({ name$: 'lmt' })
+        cl.list$({ limit$: 2, skip$: 3 }, function(err, lst) {
           Assert.ok(null == err)
           Assert.equal(0, lst.length)
           cb()
         })
       },
 
-      listlimit5skip2: function (cb) {
-        var cl = si.make({name$: 'lmt'})
-        cl.list$({limit$: 5, skip$: 2}, function (err, lst) {
+      listlimit5skip2: function(cb) {
+        var cl = si.make({ name$: 'lmt' })
+        cl.list$({ limit$: 5, skip$: 2 }, function(err, lst) {
           Assert.ok(null == err)
           Assert.equal(1, lst.length)
           cb()
         })
       },
 
-      insertUpdate: function (cb) {
+      insertUpdate: function(cb) {
         var cl = si.make$('lmt')
         cl.p1 = 'value1'
         cl.p2 = 2
-        cl.save$(function (err, foo) {
+        cl.save$(function(err, foo) {
           Assert.ok(null == err)
           Assert.ok(foo.id)
           Assert.equal(foo.p1, 'value1')
@@ -190,10 +197,10 @@ function extratest (si, done) {
           delete foo.p1
           foo.p2 = 2.2
 
-          foo.save$(function (err, foo) {
+          foo.save$(function(err, foo) {
             Assert.ok(null == err)
 
-            foo.load$({id: foo.id}, function (err, foo) {
+            foo.load$({ id: foo.id }, function(err, foo) {
               if (err) done(err)
               Assert.ok(foo.id)
               Assert.equal(foo.p1, 'value1')
@@ -204,7 +211,7 @@ function extratest (si, done) {
         })
       }
     },
-    function (err, out) {
+    function(err) {
       if (err) done(err)
       si.__testcount++
       done()
@@ -220,8 +227,8 @@ var siNative = Seneca({
   }
 })
 
-describe('mongo native tests', function () {
-  before({}, function (done) {
+describe('mongo native tests', function() {
+  before({}, function(done) {
     if (siNative.version >= '2.0.0') {
       siNative.use('entity')
     }
@@ -235,16 +242,15 @@ describe('mongo native tests', function () {
     siNative.ready(done)
   })
 
-  it('basic native', function (done) {
+  it('basic native', function(done) {
     Shared.basictest(siNative, done)
   })
 })
 
-
 var si2 = Seneca()
 
-describe('mongo regular connection test', function () {
-  before({}, function (done) {
+describe('mongo regular connection test', function() {
+  before({}, function(done) {
     if (si2.version >= '2.0.0') {
       si2.use('entity')
     }
@@ -257,16 +263,16 @@ describe('mongo regular connection test', function () {
     si2.ready(done)
   })
 
-  it('simple test', function (done) {
+  it('simple test', function(done) {
     var foo = si2.make('foo')
     foo.p1 = 'v1'
     foo.p2 = 'v2'
 
-    foo.save$(function (err, foo1) {
+    foo.save$(function(err, foo1) {
       expect(err).to.not.exist()
       expect(foo1.id).to.exist()
 
-      foo1.load$(foo1.id, function (err, foo2) {
+      foo1.load$(foo1.id, function(err, foo2) {
         expect(err).to.not.exist()
         expect(foo2).to.exist()
         expect(foo2.id).to.equal(foo1.id)
